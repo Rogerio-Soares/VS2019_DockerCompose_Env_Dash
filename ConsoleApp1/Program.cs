ff884f112a9cfeccf1ee69e19ce67024f89f66c6
Rogerio FERREIRA <FERREIRA@reside-etudes.fr>
2019-05-23 11:19:16 +0200

    VS2019 Docker-Compose environnement with dash error

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+            foreach (var c in config.AsEnumerable())
+            {
+                Console.WriteLine($"{c.Key} ==> {c.Value}");
+            }
+        }
+    }
+}
